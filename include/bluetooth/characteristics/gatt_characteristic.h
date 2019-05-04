70a28c61de90c41a9cd1db4ddd9a73c57fa8b1e4
Bartłomiej Żarnowski <thetosters@gmail.com>
2019-05-04 18:42:08 +0200

    Refactor of structure

--- include/bluetooth/characteristics/gatt_characteristic.h
+++ include/bluetooth/characteristics/gatt_characteristic.h
@@ -9,5 +9,6 @@ template<uint16_t charUID>
 class GattCharacteristic {
 public:
 protected:
-    ble_uuid_t charUuid {charUID, 0};
+    ble_uuid_t uuid {charUID, 0};
+    ble_gatts_char_handles_t handles {};
 };
